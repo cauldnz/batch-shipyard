3cd2781cfef716e4e20474b8914223748274ba0d
Chris Auld <chris.auld@auld.nz>
2017-09-20 06:57:38 +1200

    Adding lenet-demo script
    This is derived from the H2O Deepwater tests example. https://github.com/h2oai/h2o-3/blob/master/h2o-py/tests/testdir_algos/deepwater/pyunit_lenet_deepwater.py

---
+++ recipes/H2O-CPU/scripts/lenet-demo.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+import sys, os
+import h2o
+from h2o.estimators.deepwater import H2ODeepWaterEstimator
+
+h2o.connect()
+frame = h2o.import_file(("./bigdata/laptop/deepwater/imagenet/cat_dog_mouse.csv"))
+print(frame.head(5))
+model = H2ODeepWaterEstimator(epochs=100, learning_rate=1e-3, network='lenet', score_interval=0, train_samples_per_iteration=1000)
+model.train(x=[0],y=1, training_frame=frame)
+model.show()
+error = model.model_performance(train=True).mean_per_class_error()
+print(error)
+h2o.remove_all()
