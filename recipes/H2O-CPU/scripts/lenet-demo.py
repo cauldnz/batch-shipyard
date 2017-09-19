109512d1ecb1858cd819636766d74abd067c4ceb
Chris Auld <chris.auld@auld.nz>
2017-09-20 08:11:07 +1200

    Update to lenet-demo

--- recipes/H2O-CPU/scripts/lenet-demo.py
+++ recipes/H2O-CPU/scripts/lenet-demo.py
@@ -3,8 +3,8 @@ import sys, os
 import h2o
 from h2o.estimators.deepwater import H2ODeepWaterEstimator
 
-h2o.connect()
-frame = h2o.import_file(("./bigdata/laptop/deepwater/imagenet/cat_dog_mouse.csv"))
+h2o.init()
+frame = h2o.import_file(("cat_dog_mouse.csv"))
 print(frame.head(5))
 model = H2ODeepWaterEstimator(epochs=100, learning_rate=1e-3, network='lenet', score_interval=0, train_samples_per_iteration=1000)
 model.train(x=[0],y=1, training_frame=frame)
